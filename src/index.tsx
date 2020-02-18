a737975ffbad29f8042faf26a9edf8f8392cedea
Karina1213 <karinaa121213@gmail.com>
2020-02-18 11:19:17 +0200

    replace

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/project/App';
+import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 serviceWorker.unregister();
